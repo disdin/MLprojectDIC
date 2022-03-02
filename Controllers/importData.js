571dc1b311fb5ee926825cd2ea301934eec1d310
sidhant bahri <sidhantbahri01@gmail.com>
2022-03-02 12:52:32 +0530

    data to csv

--- Controllers/importData.js
+++ Controllers/importData.js
@@ -4,66 +4,63 @@ import fs from "fs";
 import { Parser } from "json2csv";
 import converter from "json-2-csv";
 
-
 const DeviceRecord = mongoose.model("DeviceRecord", schema.deviceRecords);
 
+export default function importData(req, res) {
+  const deviceId = req.body.DeviceId;
 
-export default function importData(req, res){
+  DeviceRecord.findOne({ deviceId: deviceId }, function (err, foundDevice) {
+    if (!err) {
+      if (foundDevice) {
+        const start = foundDevice.first;
+        const end = foundDevice.last;
+        var startIndex, endIndex;
+        // console.log(foundDevice.samples);
+        // DeviceRecord.find({
+        //     "deviceId": deviceId,
+        //     "samples.timeVal" : { $lte: '1645008942682'}
+        // }, function(err, test){
+        //     console.log(test.length);
+        //     for(var i=0; i<test.length; i++){
+        //         console.log(test[i]);
+        //     }
+        // });
+        const dataArray = foundDevice.samples;
+        dataArray.forEach((dataSet, index) => {
+          if (dataSet.timeVal === start) {
+            //console.log(index);
+            startIndex = index;
+          }
+          if (dataSet.timeVal === end) {
+            //console.log(index);
+            endIndex = index;
+          }
+        });
+        var requiredDataArray = [];
+        for (var i = startIndex; i <= endIndex; i++)
+          requiredDataArray.push(dataArray[i]);
 
-    const deviceId = req.body.DeviceId;
-
-    DeviceRecord.findOne({"deviceId" : deviceId}, function(err, foundDevice){
-        if(!err){
-            if(foundDevice){
-                const start = foundDevice.first;
-                const end = foundDevice.last;
-                var startIndex, endIndex;
-            // console.log(foundDevice.samples);
-            // DeviceRecord.find({
-            //     "deviceId": deviceId,
-            //     "samples.timeVal" : { $lte: '1645008942682'}
-            // }, function(err, test){
-            //     console.log(test.length);
-            //     for(var i=0; i<test.length; i++){
-            //         console.log(test[i]);
-            //     }
-            // });
-            const dataArray = foundDevice.samples;
-            dataArray.forEach((dataSet, index) => {
-                if(dataSet.timeVal === start){
-                    //console.log(index);
-                    startIndex = index;
-                }
-                if(dataSet.timeVal === end){
-                    //console.log(index);
-                    endIndex = index;
-                }
-            })
-            var requiredDataArray = [];
-            for(var i = startIndex; i<=endIndex; i++)
-                requiredDataArray.push(dataArray[i]);
-            
-            //console.log(requiredDataArray.length);
-            converter.json2csv(requiredDataArray, (err, csv) => {
-                if(err) console.log(err);
-                else {
-                    fs.writeFile('downloads/tempData.csv', csv, (err) => {
-                        if (err) throw err;
-                        console.log('The file has been saved!');
-                      });
-                }
+        //console.log(requiredDataArray.length);
+        converter.json2csv(requiredDataArray, (err, csv) => {
+          if (err) console.log(err);
+          else {
+            fs.writeFile("downloads/tempData.csv", csv, (err) => {
+              if (err) throw err;
+              else {
+                console.log("The file has been saved!");
+                res.status(200).send();
+              }
             });
-            //console.log(csv);
-             
-            
-            }
-            else{
-                console.log("device not found")
-            }
-        }
-        else{
-            console.log(err);
-        }
-    });
+          }
+        });
+        //console.log(csv);
+      } else {
+        console.log("device not found");
+        res.status(400).send();
+      }
+    } else {
+      console.log(err);
+      res.status(400).send();
+    }
+  });
 }
-
